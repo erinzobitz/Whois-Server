c7469d32c06f5826fa44b0c1403740d6dcf78ca3
Erin Zobitz <erinzobitz@gmail.com>
2012-12-03 15:38:35 -0800

    Front end & back end are working together, but the reponse does not post.

---
+++ whoisServer/app.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+require "whois"
+require "sinatra/reloader"
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  erb :index
+end
+
+
+
+post '/hi' do
+	w = Whois::Client.new
+	domain= params[:domain]
+	result = w.query(domain)
+if result.available?
+	"Your domain " + domain + " is available!"
+else
+	"Try another domain name."
+end
+end
+
